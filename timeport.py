cdc6a2758f1428ca540b9b9b8c99d2c38c9ef122
Joseph Tessitore <joetgamerman3@gmail.com>
2022-12-16 16:28:41 -0500

    Add proper credits

--- timeport.py
+++ timeport.py
@@ -2,6 +2,9 @@
 
 import fire
 
+# You can't use "from" as an argument because it's a reserved keyword
+# To make this work with fire, I had to add the underscore. Thanks
+# ChatGPT!
 def timeport(time, from_="UTC", to="UTC"):
     return (time, from_, to)
 
