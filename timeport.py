51755e985aca0b64b8597728dd0ed4744d8a6301
Joseph Tessitore <joetgamerman3@gmail.com>
2022-12-16 17:11:22 -0500

    Move format logic to new function, add min offsets

--- timeport.py
+++ timeport.py
@@ -4,6 +4,27 @@ import fire
 
 from timezone_list import timezones
 
+def convert_time(time):
+    """Converts and formats a 24 hour time"""
+    output = ""
+    am = True
+    while time > 12:
+        time -= 12
+        am = not am
+    output += str(int(time))
+    time -= int(time)
+    match time:
+        case 0.5:
+            output += ":30"
+        case 0.25:
+            output += ":15"
+        case 0.75:
+            output += ":45"
+        case 0:
+            output += ":00"
+    output += " am" if am else " pm"
+    return output
+
 # You can't use "from" as an argument because it's a reserved keyword
 # To make this work with fire, I had to add the underscore. Thanks
 # ChatGPT!
@@ -17,11 +38,7 @@ def timeport(time: str, from_="UTC", to="UTC"):
     else:
         time_num = int(time)
     result = time_num - timezones[from_] + timezones[to]
-    am = True
-    while result > 12:
-        result -= 12
-        am = not am
-    return f"{result} {'am' if am else 'pm'}"
+    return convert_time(result)
 
 if __name__ == "__main__":
     fire.Fire(timeport)
